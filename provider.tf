d7c625233c7b51d1a3fc60d79afaa0283e4fbc16
Ahmed Gameel <79502914+ahmedgamel@users.noreply.github.com>
2023-11-12 12:08:22 +0000

    Update provider with extra CSP info.tf

--- provider.tf
+++ provider.tf
@@ -1,12 +1,8 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-  }
-}
-
 provider "azurerm" {
-  features {}
+ version = "~> 2.0"
+ features {}
+ subscription_id = var.subscription_id
+ tenant_id = var.tenant_id
+ client_id = var.client_id
+ client_secret = var.client_secret
 }
