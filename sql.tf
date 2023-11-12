d36d612d16c79cc131d6e4f485ed2f4177da4cc0
Ahmed Gameel <79502914+ahmedgamel@users.noreply.github.com>
2023-11-12 12:12:07 +0000

    Update sql.tf

--- sql.tf
+++ sql.tf
@@ -1,25 +1,14 @@
-resource "azurerm_sql_server" "data_seller_sql" {
-  name                         = "DSO_SQL_UAT"
-  resource_group_name          = azurerm_resource_group.data_seller_rg.name
-  location                     = azurerm_resource_group.data_seller_rg.location
-  version                      = "12.0"
+# Azure SQL Server
+resource "azurerm_sql_server" "sql_server" {
+  name                         = var.sql_server_name
+  resource_group_name          = var.resource_group_name
+  location                     = var.location
+  version                      = "12.0" # or the latest supported version
   administrator_login          = "sqladmin"
-  administrator_login_password = data.azurerm_key_vault_secret.sql_admin_password.value
+  administrator_login_password = var.sql_admin_password
 
-  # Enabling Azure Defender for SQL
-  threat_detection_policy {
-    enabled = true
-    email_addresses = ["security@DSO.com"] # Replace with actual email addresses
-    retention_days = 30
-    use_server_default = "Disabled"
-  }
-
-  # Networking settings
-  extended_auditing_policy {
-    storage_endpoint                        = azurerm_storage_account.audit_storage.primary_blob_endpoint
-    storage_account_access_key              = azurerm_storage_account.audit_storage.primary_access_key
-    storage_account_access_key_is_secondary = false
-    retention_in_days                       = 90
+  identity {
+    type = "SystemAssigned"
   }
 
   tags = {
@@ -27,25 +16,34 @@ resource "azurerm_sql_server" "data_seller_sql" {
   }
 }
 
-# SQL firewall rule to allow specific IP ranges or Azure services
-resource "azurerm_sql_firewall_rule" "data_seller_sql_firewall" {
-  name                = "AllowAllWindowsAzureIps"
-  resource_group_name = azurerm_sql_server.data_seller_sql.resource_group_name
-  server_name         = azurerm_sql_server.data_seller_sql.name
+# Azure SQL Database (IT Database)
+resource "azurerm_sql_database" "it_database" {
+  name                = "ITDatabase" # Name of the IT database
+  resource_group_name = azurerm_sql_server.sql_server.resource_group_name
+  location            = azurerm_sql_server.sql_server.location
+  server_name         = azurerm_sql_server.sql_server.name
+
+  sku_name = "S0" # Adjust based on  performance and cost
+
+  tags = {
+    environment = "UAT"
+  }
+}
+
+# Firewall Rule to Allow Azure Services Access
+resource "azurerm_sql_firewall_rule" "allow_azure_services" {
+  name                = "AllowAzureServices"
+  resource_group_name = azurerm_sql_server.sql_server.resource_group_name
+  server_name         = azurerm_sql_server.sql_server.name
   start_ip_address    = "0.0.0.0"
-  end_ip_address      = "0.0.0.0" # Use appropriate IP ranges
+  end_ip_address      = "0.0.0.0"
 }
 
-# Retrieve SQL Server admin password from Key Vault
-data "azurerm_key_vault_secret" "sql_admin_password" {
-  name         = "sqlAdminPassword"
-  key_vault_id = azurerm_key_vault.data_seller_kv.id
-}
-
-# Configure an Azure SQL Database
-resource "azurerm_sql_database" "data_seller_sql_db" {
-  name                = "DataSellerDB"
-  resource_group_name = azurerm_sql_server.data_seller_sql.resource_group_name
-  server_name         = azurerm_sql_server.data_seller_sql.name
-  requested_service_objective_name = "S0"
+# AAD Admin for SQL Server (optional, if AAD integration is required)
+resource "azurerm_sql_active_directory_administrator" "sql_aad_admin" {
+  server_name         = azurerm_sql_server.sql_server.name
+  resource_group_name = var.resource_group_name
+  login               = "AADAdmin" # Replace with AAD Admin username
+  tenant_id           = var.tenant_id
+  object_id           = "your-aad-admin-object-id" # Replace with AAD Admin object ID
 }
