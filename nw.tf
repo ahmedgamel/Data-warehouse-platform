e87d56f96ee8fcb7e989c8e7c516863da9e91e8f
Ahmed Gameel <79502914+ahmedgamel@users.noreply.github.com>
2023-11-12 12:09:57 +0000

    Update nw with variables.tf

--- nw.tf
+++ nw.tf
@@ -1,14 +1,13 @@
-# Define a virtual network and a subnet for the data platform
-resource "azurerm_virtual_network" "ds_vnet" {
-  name                = "DSO_VNET"
-  address_space       = ["10.0.0.0/16"]
-  location            = var.location
-  resource_group_name = azurerm_resource_group.data_seller.name
+resource "azurerm_virtual_network" "vnet" {
+ name = var.vnet_name
+ address_space = [var.address_space]
+ location = var.location
+ resource_group_name = var.resource_group_name
 }
 
-resource "azurerm_subnet" "ds_subnet" {
-  name                 = "DSO_Subnet"
-  resource_group_name  = azurerm_resource_group.data_seller.name
-  virtual_network_name = azurerm_virtual_network.ds_vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
+resource "azurerm_subnet" "subnet" {
+ name = var.subnet_name
+ resource_group_name = var.resource_group_name
+ virtual_network_name = azurerm_virtual_network.vnet.name
+ address_prefixes = [var.subnet_prefix]
 }
