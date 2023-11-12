e13667f756dd73b980867db6e417f2c1eff65ae9
Ahmed Gameel <79502914+ahmedgamel@users.noreply.github.com>
2023-11-12 10:49:07 +0000

    Update rg.tf

--- rg.tf
+++ rg.tf
@@ -1,4 +1,4 @@
-resource "azurerm_resource_group" "data_seller_rg" {
+resource "azurerm_resource_group" "dso_rg" {
   name     = "DSO_RG_UAT"
-  location = "UK South"
+  location = "UK South"  # Assuming London region
 }
