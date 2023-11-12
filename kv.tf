d85ef567f4f1e32f4ef480b7215753f4965f4ce8
Ahmed Gameel <79502914+ahmedgamel@users.noreply.github.com>
2023-11-12 12:10:43 +0000

    Update kv security.tf

--- kv.tf
+++ kv.tf
@@ -1,89 +1,42 @@
-resource "azurerm_key_vault" "ds_kv" {
-  name                        = "DSO_KV_UAT"
-  location                    = var.location
-  resource_group_name         = azurerm_resource_group.data_seller.name
-  tenant_id                   = data.azurerm_client_config.current.tenant_id
-  sku_name                    = "standard"
-  soft_delete_enabled         = true
-  purge_protection_enabled    = true
+# Azure Key Vault
+resource "azurerm_key_vault" "kv" {
+  name                = var.kv_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  tenant_id           = var.tenant_id
+  sku_name            = "standard"
+
+  soft_delete_enabled        = true
+  purge_protection_enabled   = true
 
   network_acls {
-    default_action             = "Deny"
-    bypass                     = "AzureServices"
-    ip_rules                   = ["100.0.0.1/32"]
-    virtual_network_subnet_ids = [azurerm_subnet.data_seller_subnet.id]
+    default_action = "Deny"
+    bypass         = "AzureServices"
   }
 
-  access_policy {
-    tenant_id = var.tenant_id
-    object_id = var.your_object_id # Replace with the object ID of the user or service principal
-
-    key_permissions = [
-      "get",
-      "list",
-      "create",
-      "delete",
-      "update",
-      "import",
-      "backup",
-      "restore",
-      "recover",
-    ]
-
-    secret_permissions = [
-      "get",
-      "list",
-      "set",
-      "delete",
-      "recover",
-      "backup",
-      "restore",
-    ]
-
-    certificate_permissions = [
-      "get",
-      "list",
-      "create",
-      "delete",
-      "import",
-      "update",
-      "managecontacts",
-      "getissuers",
-      "listissuers",
-      "setissuers",
-      "deleteissuers",
-      "manageissuers",
-      "recover",
-    ]
-
-    storage_permissions = [
-      "get",
-      "list",
-      "delete",
-      "set",
-      "update",
-      "regeneratekey",
-      "setsas",
-      "listsas",
-      "getsas",
-      "deletesas",
-    ]
+  tags = {
+    environment = "UAT"
   }
-
-  # Additional access policies can be added here as needed
 }
 
-# Add secrets, keys, or certificates as required
-resource "azurerm_key_vault_secret" "data_seller_secret" {
-  name         = "DataSellerSecret"
-  value        = "sensitive_value" # Replace with real sensitive data or reference from a secure source
-  key_vault_id = azurerm_key_vault.data_seller_kv.id
+# Sample Secret for SQL Server Administrator Password
+resource "azurerm_key_vault_secret" "sql_admin_password" {
+  name         = "SqlAdminPassword"
+  value        = "your-strong-password-here" # Replace with actual strong password
+  key_vault_id = azurerm_key_vault.kv.id
 }
 
-# Example of adding a key
-resource "azurerm_key_vault_key" "data_seller_key" {
-  name         = "DataSellerKey"
-  key_vault_id = azurerm_key_vault.data_seller_kv.id
+# Sample Secret for Azure Data Factory
+resource "azurerm_key_vault_secret" "adf_secret" {
+  name         = "AdfSecret"
+  value        = "some-secret-value" # Replace with actual secret value
+  key_vault_id = azurerm_key_vault.kv.id
+}
+
+# Sample Key (for example, used in encryption scenarios)
+resource "azurerm_key_vault_key" "sample_key" {
+  name         = "SampleKey"
+  key_vault_id = azurerm_key_vault.kv.id
   key_type     = "RSA"
   key_size     = 2048
 
