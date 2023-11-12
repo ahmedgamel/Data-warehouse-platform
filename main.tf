b5af590e65e9199afaedbec55f869011e4c1e077
Ahmed Gameel <79502914+ahmedgamel@users.noreply.github.com>
2023-11-12 12:07:36 +0000

    Update main with NW.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,14 @@
-terraform {
-  required_version = ">= 0.13"
+module "resource_group" {
+ source = "./rg"
+ name = var.resource_group_name
+ location = var.location
+}
 
-  backend "azurerm" {
-    resource_group_name  = "DSO_RG_UAT"
-    storage_account_name = "dsostorageaccount"
-    container_name       = "tfstate"
-    key                  = "main.terraform.tfstate"
-  }
+module "network" {
+ source = "./nw"
+ vnet_name = var.vnet_name
+ subnet_name = var.subnet_name
+ address_space = var.address_space
+ subnet_prefix = var.subnet_prefix
+ resource_group_name = var.resource_group_name
 }
