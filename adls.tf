fc1eb5a62f447088527f45d47590809e43b0962e
Ahmed Gameel <79502914+ahmedgamel@users.noreply.github.com>
2023-11-12 12:13:28 +0000

    Create adls.tf

---
+++ adls.tf
@@ -0,0 +1,45 @@
+# Azure Data Lake Store Gen2 Account
+resource "azurerm_storage_account" "adls_account" {
+  name                     = var.adls_name
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  is_hns_enabled           = true
+
+  tags = {
+    environment = "UAT"
+  }
+}
+
+# Filesystem for Raw Data
+resource "azurerm_storage_data_lake_gen2_filesystem" "raw" {
+  name               = "raw"
+  storage_account_id = azurerm_storage_account.adls_account.id
+}
+
+# Filesystem for Curated Data
+resource "azurerm_storage_data_lake_gen2_filesystem" "curated" {
+  name               = "curated"
+  storage_account_id = azurerm_storage_account.adls_account.id
+}
+
+# Filesystem for Processed Data
+resource "azurerm_storage_data_lake_gen2_filesystem" "processed" {
+  name               = "processed"
+  storage_account_id = azurerm_storage_account.adls_account.id
+}
+
+# Example of setting access control at the filesystem level
+resource "azurerm_storage_data_lake_gen2_path" "example_path" {
+  path               = "/"
+  filesystem_name    = azurerm_storage_data_lake_gen2_filesystem.raw.name
+  storage_account_id = azurerm_storage_account.adls_account.id
+  resource           = "filesystem"
+
+  ace {
+    id       = "your-azure-ad-object-id"
+    type     = "user"
+    permissions = "rwx"
+  }
+}
