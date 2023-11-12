ebd12cf830a0b91ef45de2d8e13881cc5fea86be
Ahmed Gameel <79502914+ahmedgamel@users.noreply.github.com>
2023-11-12 10:59:06 +0000

    Create varibles.tf

---
+++ varibles.tf
@@ -0,0 +1,9 @@
+variable "location" {
+  description = "Location for all resources"
+  default     = "UK South"
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  default     = "DSO_RG_UAT"
+}
